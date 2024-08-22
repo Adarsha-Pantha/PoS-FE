ea994492fe1a38cfa9cc194c8a0fcb811a671896
nuastha <timilhllo@gmail.com>
2024-08-22 10:03:56 +0545

    revert back

--- src/app/page/_component/Button.jsx
+++ src/app/page/_component/Button.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "next/link";
+// import { Link } from "next/link";
 
 const Button = () => {
     return (
         
          <div class="md:flex space-x-4">
-                <Link href='../page/login' class="bg-transparent text-white border border-white px-4 py-2 rounded-lg hover:bg-white hover:text-gray-800 transition">Login</Link>
-                <Link href="../page/signup" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition">
-                    Sign Up</Link>
+                <a href='../page/login' class="bg-transparent text-white border border-white px-4 py-2 rounded-lg hover:bg-white hover:text-gray-800 transition">Login</a>
+                <a href="../page/signup" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition">
+                    Sign Up</a>
          </div>
         
     );
--- src/app/page/_component/Navbar.jsx
+++ src/app/page/_component/Navbar.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Button from "./Button";
 import logo from "./logo.png";
-import { Link } from "next/link";
+// import { Link } from "next/link";
 
 const Navbar = () => {
   return (
@@ -10,9 +10,9 @@ const Navbar = () => {
         <div class="container mx-auto flex justify-between items-center">
           {/* <!-- Logo --> */}
           <div class="flex items-center">
-            <Link href="../page" class="text-white text-2xl font-bold">
+            <a href="../page" class="text-white text-2xl font-bold">
               <Image src={logo} height={42} width={42} />
-            </Link>
+            </a>
           </div>
           {/* <!-- Buttons (Login and Sign Up) --> */}
           <Button />

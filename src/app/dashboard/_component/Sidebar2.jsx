e8f4633eee4d990c825164446e4a4b40a6359b2d
nuastha <timilhllo@gmail.com>
2024-08-25 20:29:46 +0545

    some props need to update because of error

--- src/app/dashboard/_component/Sidebar2.jsx
+++ src/app/dashboard/_component/Sidebar2.jsx
@@ -16,23 +16,24 @@ const Sidebar2 = () => {
   const [active, setActive] = useState(true);
   const [open, setOpen] = useState(false);
   const menu = [
-    { name: "Dashboard", link: "/dashboard", icon: <BiSolidDashboard /> },
-    { name: "Inventory", link: "/dashboard/inventory", icon: <MdInventory /> },
-    { name: "Top Selling", link: "/dashboard/top-selling", icon: <MdOutlineSell /> },
+    { id: 1, name: "Dashboard", link: "/dashboard", icon: <BiSolidDashboard /> },
+    { id: 2, name: "Inventory", link: "/dashboard/inventory", icon: <MdInventory /> },
+    { id: 3, name: "Top Selling", link: "/dashboard/top-selling", icon: <MdOutlineSell /> },
     {
       name: "Report",
+      id: 4,
       link: "/dashboard/report",
       icon: <HiOutlineDocumentReport />,
       submenu: true,
       submenuItems: [
-        { name: "Product Summary", link: "/dashboard/report/product-summary" },
-        { name: "Purchase", link: "/dashboard/report/purchase" },
-        { name: "Sales", link: "/dashboard/report/sales" },
+        { subId: 1, name: "Product Summary", link: "/dashboard/report/product-summary" },
+        { subId: 2, name: "Purchase", link: "/dashboard/report/purchase" },
+        { subId: 3, name: "Sales", link: "/dashboard/report/sales" },
       ]
     },
-    { name: "Customers", link: "/dashboard/customers", spacing: true, icon: <PiUserFocusThin /> },
-    { name: "Settings", link: "/dashboard/settings", icon: <IoSettingsOutline /> },
-    { name: "Logout", link: "/dashboard/logout", icon: <IoIosLogOut /> },
+    { id: 5, name: "Customers", link: "/dashboard/customers", spacing: true, icon: <PiUserFocusThin /> },
+    { id: 6, name: "Settings", link: "/dashboard/settings", icon: <IoSettingsOutline /> },
+    { id: 7, name: "Logout", link: "/dashboard/logout", icon: <IoIosLogOut /> },
   ];
 
   return (
@@ -50,29 +51,32 @@ const Sidebar2 = () => {
           <input type="text" placeholder="Search" className={`text-black text-base bg-transparent focus:outline-none w-full ${!active && "hidden"}`} />
         </div>
         <ul className="pt-6">
-          {menu.map((menu, index) => (
-            <>
-              <li key={index} className={`text-white text-sm flex items-center gap-x-2 cursor-pointer p-1.5 duration-200 hover:bg-zinc-600 rounded-md ${menu.spacing ? 'mt-9' : 'mt-2'}`} >
-                <span className={`block float-left duration-300 ${!active ? 'text-3xl': 'text-2xl'}`} onClick={()=>setActive(!active)}>
-                  {menu.icon}
-                </span>
-                <a href={menu.link} className={`text-white text-base font-medium flex-1 duration-500 ${!active && "hidden"}`} >
-                  {menu.name}
-                </a>
-                {menu.submenu && active && (<MdKeyboardArrowDown className={`${open && 'rotate-180'}`} onClick={() => setOpen(!open)} />)}
-              </li>
-              {menu.submenu && open && active && (
-                <ul className={`text-white text-sm cursor-pointer p-2 rounded-md ml-3 bg-slate-600`} >
-                  {menu.submenuItems.map((Items, index) => (
-                    <li key={index} className={`text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 rounded-md hover:bg-slate-400`} >
-                     <a href={Items.link}>{Items.name}</a> 
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </>
+  {menu.map((menuItem) => (
+    <React.Fragment key={menuItem.id}>
+      <li className={`text-white text-sm flex items-center gap-x-2 cursor-pointer p-1.5 duration-200 hover:bg-zinc-600 rounded-md ${menuItem.spacing ? 'mt-9' : 'mt-2'}`}>
+        <span className={`block float-left duration-300 ${!active ? 'text-3xl': 'text-2xl'}`} onClick={() => setActive(!active)}>
+          {menuItem.icon}
+        </span>
+        <a href={menuItem.link} className={`text-white text-base font-medium flex-1 duration-500 ${!active && "hidden"}`}>
+          {menuItem.name}
+        </a>
+        {menuItem.submenu && active && (
+          <MdKeyboardArrowDown className={`${open && 'rotate-180'}`} onClick={() => setOpen(!open)} />
+        )}
+      </li>
+      {menuItem.submenu && open && active && (
+        <ul className={`text-white text-sm cursor-pointer p-2 rounded-md ml-3 bg-slate-600`}>
+          {menuItem.submenuItems.map((submenuItem) => (
+            <li key={submenuItem.subId} className={`text-white text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 rounded-md hover:bg-slate-400`}>
+              <a href={submenuItem.link}>{submenuItem.name}</a>
+            </li>
           ))}
         </ul>
+      )}
+    </React.Fragment>
+  ))}
+</ul>
+
       </div>
     </aside>
   );

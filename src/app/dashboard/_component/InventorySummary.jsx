54cd8f41b347d795d35b7e8b521334fd1778946e
nuastha <timilhllo@gmail.com>
2024-08-28 07:15:22 +0545

    refactor to be dynamic

--- src/app/dashboard/_component/InventorySummary.jsx
+++ src/app/dashboard/_component/InventorySummary.jsx
@@ -1,37 +1,55 @@
-import React from 'react'
+import React from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { TbCategory } from "react-icons/tb";
 
+const summaryData = [
+  {
+    icon: IoPersonCircleOutline,
+    value: 868,
+    label: "Quantity in Hand",
+    iconColor: "text-blue-500",
+  },
+  {
+    icon: TbCategory,
+    value: 200,
+    label: "To be received",
+    iconColor: "text-green-300",
+  },
+];
 
+const InventorySummaryItem = ({ icon: Icon, value, label, iconColor }) => {
+  return (
+    <div className="flex flex-col text-center gap-y-2">
+      <div className="flex justify-center">
+        <Icon className={`text-3xl ${iconColor}`} />
+      </div>
+      <div>
+        <p>{value}</p>
+        <h4>{label}</h4>
+      </div>
+    </div>
+  );
+};
 
 const InventorySummary = () => {
   return (
-    <div className='bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-25 border border-gray-100 border-opacity-35 flex flex-col gap-y-4 p-5 w-5/12'>
+    <div className="bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-25 border border-gray-100 border-opacity-35 flex flex-col gap-y-4 p-5 w-5/12">
       <div>
-        <h2 className='capitalize text-lg'>inventory Summary</h2>
+        <h2 className="capitalize text-lg">Inventory Summary</h2>
       </div>
-      <div className='flex justify-between w-auto'>
-        <div className='flex flex-col text-center gap-y-2'>
-          <div className='flex justify-center'>
-          <IoPersonCircleOutline className='text-3xl text-blue-500' />
-          </div>
-          <div>
-            <p>868</p>
-            <h4>Quantity in Hand</h4>
-          </div>
-        </div>
-        <div className='flex flex-col text-center gap-y-2'>
-          <div className='flex justify-center'>
-          <TbCategory className='text-3xl text-green-300' />
-          </div>
-          <div>
-            <p>200</p>
-            <h4>To be received</h4>
-          </div>
-        </div>
+      <div className="flex justify-between w-auto">
+        {summaryData.map((item, index) => (
+          <InventorySummaryItem
+            key={index}
+            icon={item.icon}
+            value={item.value}
+            label={item.label}
+            iconColor={item.iconColor}
+          />
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default InventorySummary
+export default InventorySummary;
